c625f986186b2709eadcf1cdfec9a75f72dc38f3
ABDR4FI3 <its.abdrafi3@gmail.com>
2024-04-02 12:43:54 +0000

    Working Version 1: in this version the basics like User and "Demandes" Crud and authentication are working and first functions of JWT also are implemented Working Version 2 : more functionalities using JWT like -extractUsername -extractClaims -isTokenValid -isTokenExpired

--- src/main/java/org/example/springpfa/Services/JWTUtils.java
+++ src/main/java/org/example/springpfa/Services/JWTUtils.java
@@ -1,6 +1,7 @@
 package org.example.springpfa.Services;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.example.springpfa.entities.User;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.function.Function;
 
 @Component
 public class JWTUtils {
@@ -17,17 +22,58 @@ public class JWTUtils {
     private static final long ExpirationTime = 3_600_000 ; // how many milliseconds in 1 hour
 
     public JWTUtils(){
+        // Hardcoded secret string (should be kept secure and not hardcoded in real-world scenarios)
         String secretString = "5454878c454sc54s575c7s5c4s5c4c5s75cs5c7sc4s5c4s54cs57s57scs5754151sc";
+
+        // Decode the secret string from Base64 encoding into raw bytes
         byte[] keyBytes = Base64.getDecoder().decode((secretString.getBytes(StandardCharsets.UTF_8)));
-        this.secretKey =  new SecretKeySpec(keyBytes,"HmacSHA256");
+
+        // Create a SecretKeySpec object using the decoded key bytes and the HmacSHA256 algorithm
+        this.secretKey = new SecretKeySpec(keyBytes, "HmacSHA256");
     }
 
+
     public String generateToken(User user){
+        // Start building the JWT token using the Jwts.builder() method
         return Jwts.builder()
+                // Set the subject of the token to the username of the user
                 .setSubject(user.getUsername())
+                // Set the issued date of the token to the current time plus the expiration time
                 .setIssuedAt(new Date(System.currentTimeMillis() + ExpirationTime))
+                // Sign the token with the secret key specified in the JWTUtils class
+                .signWith(secretKey)
+                // Generate the compact serialized form of the JWT token
+                .compact();
+    }
+    public String refreshToken(HashMap<String, Object> claims , User user){
+        return Jwts.builder()
+                .addClaims(claims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()) )
+                .setExpiration(new Date(System.currentTimeMillis() + ExpirationTime))
                 .signWith(secretKey)
                 .compact();
     }
+    //
+    public String extractUserName(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+    //
+    private <T> T extractClaims(String token, Function<Claims,T> claimsTFunction){
+        return claimsTFunction.apply(Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody());
+    }
+    public boolean isTokenValid(String token, User user){
+        final String userName = extractUserName(token);
+        return (userName.equals(user.getUsername())) && !isTokenExpired(token);
+    }
+
+    public  boolean isTokenExpired(String token){
+        return extractClaims(token,Claims::getExpiration).before(new Date());
+
+    }
+
+
+
+
 }
 
